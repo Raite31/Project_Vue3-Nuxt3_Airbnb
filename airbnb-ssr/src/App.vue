986f43d8ec3bdb71f0f23cf75d5010da664a94ff
李嘉胜 <2330165939@qq.com>
2024-01-16 00:04:08 +0800

    feat: 使用eslint

--- airbnb-ssr/src/App.vue
+++ airbnb-ssr/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: 李嘉胜 2330165939@qq.com
  * @Date: 2024-01-14 16:52:03
  * @LastEditors: 李嘉胜 2330165939@qq.com
- * @LastEditTime: 2024-01-15 00:10:42
+ * @LastEditTime: 2024-01-16 00:00:56
  * @FilePath: /Project_Vue3-Nuxt3_Airbnb/airbnb-ssr/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,6 +10,8 @@
 import HelloWorld from './components/HelloWorld.vue';
 import { useRouter } from 'vue-router';
 const router = useRouter();
+const aa = '';
+console.log(aa);
 </script>
 
 <template>
