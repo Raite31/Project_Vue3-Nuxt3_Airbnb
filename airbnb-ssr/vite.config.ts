6d5ccb13e82bca94e102d3a5a3e968b4ffb92351
李嘉胜 <2330165939@qq.com>
2024-01-15 23:48:44 +0800

    feat: 全局、局部使用Element-plus组件

--- airbnb-ssr/vite.config.ts
+++ airbnb-ssr/vite.config.ts
@@ -1,22 +1,39 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+// 实现按需引入组件的两个包
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // 路径配置start
-import { resolve } from 'path';
+// import { resolve } from 'path';
 
-const pathResolve = (dir: string) => {
-	return resolve(__dirname, '.', dir);
-};
+// const pathResolve = (dir: string) => {
+// 	return resolve(__dirname, '.', dir);
+// };
 
-const alias: Record<string, string> = {
-	'@': pathResolve('src'),
-};
+// const alias: Record<string, string> = {
+// 	'@': pathResolve('src'),
+// };
 // 路径配置end
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()],
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()],
+		}),
+	],
 	resolve: {
-		alias,
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
 	},
 });
