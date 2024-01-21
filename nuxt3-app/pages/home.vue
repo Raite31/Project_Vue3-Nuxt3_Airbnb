600422921e307fc7142a84fceda6b65d1162f311
李嘉胜 <2330165939@qq.com>
2024-01-22 07:32:11 +0800

    feat: home页-test

---
+++ nuxt3-app/pages/home.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: 李嘉胜 2330165939@qq.com
+ * @Date: 2024-01-21 15:58:07
+ * @LastEditors: 李嘉胜 2330165939@qq.com
+ * @LastEditTime: 2024-01-21 16:00:02
+ * @FilePath: /Project_Vue3-Nuxt3_Airbnb/nuxt3-app/pages/home.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup lang="ts">
+// const { data: count } = await useFetch('/api/count')
+</script>
+
+<template>
+  <p>Page visits:</p>
+</template>
